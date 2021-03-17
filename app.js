a6ab5a9c94431d137c66d732a250b2cb36259344
Sellers Crisp <sellershcrisp@gmail.com>
2021-03-17 12:09:33 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 /** BizTime express application. */
 
-
 const express = require("express");
-
 const app = express();
-const ExpressError = require("./expressError")
+
+const ExpressError = require("./utils/expressError");
+const cRoutes = require('./routes/companies');
+const iRoutes = require('./routes/invoices');
 
 app.use(express.json());
 
+app.use('/companies', cRoutes);
+app.use('/invoices', iRoutes);
 
 /** 404 handler */
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
 
 /** general error handler */
-
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
